33977e041827eb2208ee741936f05ee1b19c9f1d
Colton Freeman <91649148+coltonfreeman@users.noreply.github.com>
2021-11-27 21:54:30 -0800

    updated week1 elements

--- week1/element.js
+++ week1/element.js
@@ -1,3 +1,2 @@
-let element = document.getElementById("idName");
-console.log("idName")
-element.innerHTML += "Welcome to Week 1"
+var element = document.getElementById("Dallas");
+console.log(element)
