77c7c4bb8eacbae8915e85ecbb38cc94a5482b76
Colton Freeman <91649148+coltonfreeman@users.noreply.github.com>
2021-12-15 14:37:55 -0800

    updated week4

--- week4/Event.js
+++ week4/Event.js
@@ -1,11 +1,13 @@
-const square = document.createElement('div');
-square.style.width = "400px";
-square.style.height = "400px";
-square.style.backgroundColor = "white";
-square.style.margin = "0 auto";
-square.style.marginTop = "20px";
-square.style.border = "5px solid black";
-document.body.appendChild(square);
+const canvas = document.createElement('div');
+canvas.style.width = "400px";
+canvas.style.height = "400px";
+canvas.style.backgroundColor = "white";
+canvas.style.backgroundColor = "white";
+canvas.style.backgroundColor = "white";
+canvas.style.margin = "0 auto";
+canvas.style.marginTop = "20px";
+canvas.style.border = "5px solid black";
+document.body.appendChild(canvas);
 //White to start as a base color.
 for (let i = 0; i < 20; i++) {
     const h1 = document.createElement("h1");
@@ -14,49 +16,49 @@ for (let i = 0; i < 20; i++) {
 }
 //Black upon complete loading of the HTML page *you might have to look up how to do this*
 window.addEventListener("load", function() {
-    square.style.backgroundColor = "black";
+    canvas.style.backgroundColor = "black";
 });
 //Green when the mouse hovers over the square
-square.addEventListener("mouseover", function() {
-    square.style.backgroundColor = "green";
+canvas.addEventListener("mouseover", function() {
+    canvas.style.backgroundColor = "green";
 });
 //Yellow when the mouse button is held down over the square
-square.addEventListener("mousedown", function() {
-    square.style.backgroundColor = "yellow";
+canvas.addEventListener("mousedown", function() {
+    canvas.style.backgroundColor = "yellow";
 });
 //Blue when the mouse button is let go over the square
-square.addEventListener("mouseup", function() {
-    square.style.backgroundColor = "blue";
+canvas.addEventListener("mouseup", function() {
+    canvas.style.backgroundColor = "blue";
 });
 //Red when the mouse is double clicked in the square
-square.addEventListener("dblclick", function() {
-    square.style.backgroundColor = "red";
+canvas.addEventListener("dblclick", function() {
+    canvas.style.backgroundColor = "red";
 });
 //Purple when the mouse scroll is used somewhere in the window (not just over the square). 
 window.addEventListener("wheel", function() {
-    square.style.backgroundColor = "purple";
+    canvas.style.backgroundColor = "purple";
 })
 
 function setSquareBackgroundColor(color) {
-    square.style.backgroundColor = color;
+    canvas.style.backgroundColor = color;
 };
 
 window.addEventListener('keydown', function(event) {
     switch (event.which) {
         case 82:
-            setSquareBackgroundColor('red');
+            setCanvasBackgroundColor('red');
             break;
         case 71:
-            setSquareBackgroundColor('green');
+            setCanvasBackgroundColor('green');
             break;
         case 66:
-            setSquareBackgroundColor('blue');
+            setCanvasBackgroundColor('blue');
             break;
         case 89:
-            setSquareBackgroundColor('yellow');
+            setCanvasBackgroundColor('yellow');
             break;
         case 80:
-            setSquareBackgroundColor('purple');
+            setCanvasBackgroundColor('purple');
             break;
     }
 });
