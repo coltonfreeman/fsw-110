de7de87e8361b28f16a0057e62d237dc0b496ccf
Colton Freeman <91649148+coltonfreeman@users.noreply.github.com>
2021-12-18 18:15:29 -0800

    updated week4

--- week4/Event.js
+++ week4/Event.js
@@ -1,13 +1,13 @@
-const canvas = document.createElement('div');
-canvas.style.width = "400px";
-canvas.style.height = "400px";
-canvas.style.backgroundColor = "white";
-canvas.style.backgroundColor = "white";
-canvas.style.backgroundColor = "white";
-canvas.style.margin = "0 auto";
-canvas.style.marginTop = "20px";
-canvas.style.border = "5px solid black";
-document.body.appendChild(canvas);
+//For this assignment you will create a square and have the color of that square change by interacting with different HTML Events.
+const square = document.createElement('div');
+square.style.width = "400px";
+square.style.height = "400px";
+square.style.backgroundColor = "white";
+square.style.margin = "0 auto";
+square.style.marginTop = "20px";
+square.style.border = "5px solid black";
+document.body.appendChild(square);
+
 //White to start as a base color.
 for (let i = 0; i < 20; i++) {
     const h1 = document.createElement("h1");
@@ -16,49 +16,55 @@ for (let i = 0; i < 20; i++) {
 }
 //Black upon complete loading of the HTML page *you might have to look up how to do this*
 window.addEventListener("load", function() {
-    canvas.style.backgroundColor = "black";
+    square.style.backgroundColor = "black";
 });
+
 //Green when the mouse hovers over the square
-canvas.addEventListener("mouseover", function() {
-    canvas.style.backgroundColor = "green";
+square.addEventListener("mouseover", function() {
+    square.style.backgroundColor = "green";
 });
+
 //Yellow when the mouse button is held down over the square
-canvas.addEventListener("mousedown", function() {
-    canvas.style.backgroundColor = "yellow";
+square.addEventListener("mousedown", function() {
+    square.style.backgroundColor = "yellow";
 });
+
 //Blue when the mouse button is let go over the square
-canvas.addEventListener("mouseup", function() {
-    canvas.style.backgroundColor = "blue";
+square.addEventListener("mouseup", function() {
+    square.style.backgroundColor = "blue";
 });
+
 //Red when the mouse is double clicked in the square
-canvas.addEventListener("dblclick", function() {
-    canvas.style.backgroundColor = "red";
+square.addEventListener("dblclick", function() {
+    square.style.backgroundColor = "red";
 });
 //Purple when the mouse scroll is used somewhere in the window (not just over the square). 
 window.addEventListener("wheel", function() {
-    canvas.style.backgroundColor = "purple";
+    square.style.backgroundColor = "purple";
 })
 
 function setSquareBackgroundColor(color) {
-    canvas.style.backgroundColor = color;
+    square.style.backgroundColor = color;
 };
+//*Try adding more content to your page with JS if you can not scroll.
 
+//You should also be able to press the first letter of the colors (typing "r" on the keyboard for "red", for example) and have the box change to that color
 window.addEventListener('keydown', function(event) {
     switch (event.which) {
         case 82:
-            setCanvasBackgroundColor('red');
+            setSquareBackgroundColor('red');
             break;
         case 71:
-            setCanvasBackgroundColor('green');
+            setSquareBackgroundColor('green');
             break;
         case 66:
-            setCanvasBackgroundColor('blue');
+            setSquareBackgroundColor('blue');
             break;
         case 89:
-            setCanvasBackgroundColor('yellow');
+            setSquareBackgroundColor('yellow');
             break;
         case 80:
-            setCanvasBackgroundColor('purple');
+            setSquareBackgroundColor('purple');
             break;
     }
 });
